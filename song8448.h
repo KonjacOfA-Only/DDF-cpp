858367dbd3dbced11bb066fe8dcfeca9d81abbac
KonjacOfA-Only <47107824+KonjacOfA-Only@users.noreply.github.com>
2019-03-03 15:27:20 +0800

    Add files via upload

---
+++ song8448.h
@@ -0,0 +1,43 @@
+/*
+Copyright 2019 BND Studio : DDF for C++ port
+Early but stable version , it's another option to use DDF
+Import Headfile :
+	#include <song8448.h> 
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <Windows.h>
+
+#define inf 10000000000
+
+van() {
+	system("C:/ddf/extends/van.bat");
+	printf("Done.");
+}
+
+hop() {
+	printf("lover lover f**ker f**ker lover");	getchar();
+	printf("§®§Ú§Ý§à , §à§Ò§Ú§é§Ñ§ê §Ý§Ú §Þ§Ö §à§ë§Ö?");	getchar();
+	printf("§¢§Ö§Û§Ò§Ú , §ã§ì§Ò§Ý§Ú§é§Ñ§ê §Ý§Ú §Þ§Ö §ß§à§ë§Ö§Þ?");	getchar();
+	printf("§¬§Ñ§â§Ñ§Û §Ò§Ñ§Ó§ß§à §ß§ñ§Þ§Ñ §Õ§Ñ §Ò§ì§â§Ù§Ñ§ê.");	getchar();
+	printf("§¬§Ñ§Ü §ë§Ö §ã§ä§Ñ§ß§Ö §Þ§ß§à§Ô§à §Þ§ì§â§Õ§Ñ§ê!");	getchar();
+	printf("§·§à§á, §Ú §Ó§Ý§Ú§Ù§Ñ §Þ§Ñ§Ý§Ü§à §á§à §Þ§Ñ§Ý§Ü§à.");	getchar();
+	printf("§·§à§á, §Õ§Ó§Ú§Ø§Ú §ã§Ö §Ò§Ñ§Ó§ß§à §Ú §Ò§Ñ§Ó§ß§à.");	getchar();
+	printf("§·§à§á, §Ú §Ó§Ü§Ñ§â§Ñ§Û §Ô§à §ä§à§é§ß§à §ã§Ö§Ô§Ñ.");	getchar();
+	printf("§²§Ú§ä§ì§Þ§Ñ §ß§Ñ §Ý§ð§Ò§à§Ó§ä§Ñ! ");	getchar();
+	printf("§·§à§á, §Ú §Ó§Ý§Ú§Ù§Ñ §Þ§Ñ§Ý§Ü§à §á§à §Þ§Ñ§Ý§Ü§à.");	getchar();
+	printf("§·§à§á, §Õ§Ó§Ú§Ø§Ú §ã§Ö §Ò§Ñ§Ó§ß§à §Ú §Ò§Ñ§Ó§ß§à.");	getchar();
+	printf("§·§à§á, §Ú §Ó§Ü§Ñ§â§Ñ§Û §Ô§à §ä§à§é§ß§à §ã§Ö§Ô§Ñ.");	getchar();
+	printf("§²§Ú§ä§ì§Þ§Ñ §ß§Ñ §Ý§ð§Ò§à§Ó§ä§Ñ! ");	getchar();
+	printf("§·§à§á");
+}
+
+//to be continued
+
+/*
+DDF for C++ HeadFiles at Github
+Github @KonjacOfA-Only
+https://github.com/KonjacOfA-Only/DDF-cpp/releases
+*/ 
